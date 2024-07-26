975630fa843b09a54b23c0f2839039fd848476b3
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-07-26 13:29:32 -0300

    cria e desenvolve o component de Pessoa e exporta para uso no codigo de App.js

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
 
 function App() {
   const name = "Rafael";
@@ -9,6 +10,12 @@ function App() {
       <HelloWorld />
       <SayMyName name="João" />
       <SayMyName name={name} />
+      <Pessoa
+        nome="Gabriel"
+        idade="26"
+        profissao="Programador"
+        foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg1MndL-Xp1JcnqaB0YOqTp6zDjrwYyGKsPA&s"
+      />
     </div>
   );
 }
