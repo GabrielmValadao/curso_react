798688e7c2e09385ed70858b6f261e16d9b6e9f0
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-07-26 10:14:15 -0300

    altera a imagem do jsx

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   const name = "Gabriel";
   const url =
-    "https://www.freecodecamp.org/portuguese/news/content/images/2023/03/Ekran-Resmi-2019-11-18-18.08.13.png";
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg1MndL-Xp1JcnqaB0YOqTp6zDjrwYyGKsPA&s";
   return (
     <div className="App">
       <h2>Alterando o JSX</h2>
