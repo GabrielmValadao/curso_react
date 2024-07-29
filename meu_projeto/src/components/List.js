4e664c46ee94423d099142d868d4065f32811235
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-07-29 20:21:14 -0300

    utiliza mais props para verificação de dados e mensagem de error

--- meu_projeto/src/components/List.js
+++ meu_projeto/src/components/List.js
@@ -5,9 +5,9 @@ function List() {
     <>
       <h1>Minha lista</h1>
       <ul>
-        <Item marca="Ferrari" />
-        <Item marca="Porsche" />
-        <Item marca="Tesla" />
+        <Item marca="Ferrari" ano_lancamento={1870} />
+        <Item marca="Porsche" ano_lancamento={1968} />
+        <Item marca="Tesla" ano_lancamento={2010} />
       </ul>
     </>
   );
