58fede978256629d84263b0ffa9ffa3c8c973a42
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-07-29 20:59:32 -0300

    feat: desenvolve components evento e form para uso de eventos e implementa o uso no arquivo app.js

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,26 +1,15 @@
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
-import SayMyName from "./components/SayMyName";
-import Pessoa from "./components/Pessoa";
-import Frase from "./components/Frase";
-import List from "./components/List";
+import Evento from "./components/Evento";
+import Form from "./components/Form";
 
 function App() {
   const name = "Rafael";
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <HelloWorld />
-      <SayMyName name="João" />
-      <SayMyName name={name} />
-      <Pessoa
-        nome="Gabriel"
-        idade="26"
-        profissao="Programador"
-        foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg1MndL-Xp1JcnqaB0YOqTp6zDjrwYyGKsPA&s"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
