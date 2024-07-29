50e3841e9dec94a2f492568bb5ec3800d3d9011e
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-07-29 19:54:55 -0300

    faz estilização pelo componente e pelo index.css

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
+import Frase from "./components/Frase";
 
 function App() {
   const name = "Rafael";
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />
       <HelloWorld />
       <SayMyName name="João" />
       <SayMyName name={name} />
