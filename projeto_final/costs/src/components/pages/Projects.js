94f1e7527b0468a00bbfe2ef3be1fa78a2f4f388
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-08-19 10:09:22 -0300

    feat: realiza o retorno dos dados cadastrados pelo usuario na pagina de novo projeto e envia para a pagina de  'meus projetos'

--- projeto_final/costs/src/components/pages/Projects.js
+++ projeto_final/costs/src/components/pages/Projects.js
@@ -5,13 +5,32 @@ import sytles from "./Projects.module.css";
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+import { useState, useEffect } from "react";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch("http://localhost:3001/projects", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div className={sytles.project_container}>
       <div className={sytles.title_container}>
@@ -20,7 +39,8 @@ function Projects() {
       </div>
       {message && <Message type="sucess" msg={message} />}
       <Container customClass="start">
-        <p>Projetos</p>
+        {projects.length > 0 &&
+          projects.map((project) => <ProjectCard name={project.name} />)}
       </Container>
     </div>
   );
---
+++ projeto_final/costs/src/components/project/ProjectCard.js
@@ -0,0 +1,10 @@
+// importa os dados do backend cadastrados pelo usuario
+
+import styles from "./ProjectCard.module.css";
+// import { BsPencil, BsFillTrashFill } from "react-icons-bs";
+
+function ProjectCard({ id, name, budget, categorie, handleRemove }) {
+  return <p>{name}</p>;
+}
+
+export default ProjectCard;
