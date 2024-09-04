07ffa89c05ba685e0f27a0ca8667ddc9f06cd134
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-04 15:27:24 -0300

    cria pasta da segunda seção do curso e começa o desenvolvimento de components

---
+++ aulas-udemy/2_fundamentos/fundamentos/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+// components
+import FirstComponent from './components/FirtsComponent';
+
+// styles 
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Fundamentos React</h1>
+      <FirstComponent /> 
+    </div>
+  );
+}
+
+export default App;
