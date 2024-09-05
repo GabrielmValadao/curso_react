e8ba4ac0612c6a6b3a5909750c3276473e7a9def
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-05 15:29:58 -0300

    desenvolve o challenge da seção 2 do curso

--- aulas_udemy/2_fundamentos/fundamentos/src/App.jsx
+++ aulas_udemy/2_fundamentos/fundamentos/src/App.jsx
@@ -7,6 +7,7 @@ import Events from './components/Events';
 
 // styles 
 import './App.css'
+import Challenge from './components/Challenge';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
       <FirstComponent /> 
       <TemplateExpressions/>
       <Events/>
+      <Challenge/>
       
     </div>
   );
