846bb3d1ac11f87e82af9906ad1038bd8225db4d
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-10 15:25:20 -0300

    desenvolve loop em array de objetos

--- aulas_udemy/3_avançando_react/avançando_react/src/App.jsx
+++ aulas_udemy/3_avançando_react/avançando_react/src/App.jsx
@@ -17,6 +17,12 @@ function App() {
 
   const [userName] = useState("José")
 
+  const cars = [
+    {id: 1, brand:"BYD", color: "Cinza", newCar: true, km: 0 },
+    {id: 2, brand:"Ferrari", color: "Vermelho", newCar: true, km: 0 },
+    {id: 3, brand:"Porsche", color: "Laranja", newCar: false, km: 15000 }
+  ]
+
   return (
     <div>
       <h1>Seção 3 - Avançando em React</h1>
@@ -35,11 +41,24 @@ function App() {
       {/* props */}
       <ShowUserName name="Gabriel" />
       <ShowUserName name={userName} />
+
       {/* destructuring */}
       <CarDetails brand="Chevrolet" km={100} color="Vermelho"/>
+
+      {/* Reaproveitamento de componentes */}
       <CarDetails brand="Ford" km={0} color="Cinza" newCar={true} />
       <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false} />
       <CarDetails brand="Tesla" km={0} color="Azul espacial" newCar={true}/>
+      
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails 
+        brand={car.brand} 
+        color={car.color} 
+        km={car.km} 
+        newCar={car.newCar} 
+        />
+      ))}
     </div>
   )
 }
