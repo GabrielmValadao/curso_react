73d72a4025b42cbd7dffcbffec580fbb0ec78982
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-10 09:45:15 -0300

    implementa o uso de key na list

--- aulas_udemy/3_avançando_react/avançando_react/src/components/ListRender.jsx
+++ aulas_udemy/3_avançando_react/avançando_react/src/components/ListRender.jsx
@@ -3,12 +3,35 @@ import { useState } from "react"
 const ListRender = () => {
     const [list] = useState(["Gabriel", "Milena" , "Silvana"])
 
+    const [users] = useState([
+      {
+        id: 1,
+        name: "Gabriel",
+        age: 26
+      },
+      {
+        id: 2,
+        name: "Milena",
+        age: 25
+      },
+      {
+        id: 3,
+        name: "Silvana",
+        age: 53
+      }
+    ])
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
             ))}
+        </ul>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name} - {user.age}</li>
+          ))}
         </ul>
     </div>
   )
