47bb6ab434771cb957ff6aed0febd15d2733752f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-10 10:26:09 -0300

    adiciona exemplo de if ternário

--- aulas_udemy/3_avançando_react/avançando_react/src/components/ConditionalRender.jsx
+++ aulas_udemy/3_avançando_react/avançando_react/src/components/ConditionalRender.jsx
@@ -3,11 +3,24 @@ import { useState } from "react"
 const ConditionalRender = () => {
     const [x] = useState(false)
 
+    const [name, setName] = useState("Silvana")
+
   return (
     <div>
         <h2>Este item será exibido?</h2>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora o x é falso</p>}
+        {/* if ternário */}
+        {name === "Gabriel" ? (
+            <div>
+                <p>O nome é Gabriel</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não encontrado</p>
+            </div>
+        ) }
+        <button onClick={() => setName("Gabriel")}>Clique aqui</button>
     </div>
   )
 }
