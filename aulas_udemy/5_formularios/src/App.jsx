9e0316a23c1a2193c967b7a5a036fe70e0e78921
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-13 17:01:48 -0300

    feat: cria o primeiro formulario

---
+++ aulas_udemy/5_formularios/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import MyForm from './components/MyForm'
+
+function App() {
+
+  return (
+      <div>
+        <h1>Formulários</h1>
+        <MyForm/>
+      </div>
+      
+    
+  )
+}
+
+export default App
