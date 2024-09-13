066a695fd0e98ff0bd7e96ca37b4473492340b88
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-13 15:45:27 -0300

    feat: desenvolve exemplo de css global e css somente de component

--- aulas_udemy/4_css_react/src/App.jsx
+++ aulas_udemy/4_css_react/src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
   return (
       <div className='App'>
+        {/* CSS global */}
         <h1>React com CSS</h1>
+
+        {/* CSS de component */}
+        <MyComponent />
+        <p>Este paragrafo é do App.js</p>
+
       </div>
   )
 }
---
+++ aulas_udemy/4_css_react/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h2>Css de component</h2>
+        <p>Este é um paragrafo</p>
+        <p className='my-comp-paragraph'>Este é outro paragrafo de component</p>
+    </div>
+  )
+}
+
+export default MyComponent
