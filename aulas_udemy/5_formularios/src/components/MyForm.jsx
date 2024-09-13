f8f7d0ab21eb7f5c03a6604e569653150cb73d39
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-13 17:15:55 -0300

    adiciona exemplo de manipulação de valores

--- aulas_udemy/5_formularios/src/components/MyForm.jsx
+++ aulas_udemy/5_formularios/src/components/MyForm.jsx
@@ -1,21 +1,42 @@
+import { useState } from 'react'
+
 import './MyForm.css'
 
 const MyForm = () => {
+    // 3 - gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+        console.log(name)
+    }
+
   return (
     <div>
         {/* 1 - criação de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name='name' placeholder='Digite seu nome' />
+                <input 
+                type="text" 
+                name='name' 
+                placeholder='Digite seu nome' 
+                onChange={handleName}/>
             </div>
 
             {/* 2 - Label envolvendo input */}
             <label>
                 <span>E-mail:</span>
-                <input type="email" name='email' placeholder='Digite o seu email' />
+                <input 
+                type="email" 
+                name='email' 
+                placeholder='Digite o seu email' />
             </label>
-            <input type="submit" value='Enviar'/>
+
+            <input 
+            type="submit" 
+            value='Enviar'/>
         </form>
     </div>
   )
