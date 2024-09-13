4b69c16f09de66028cf35871d738eee83e1cc5db
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-13 15:56:29 -0300

    style: desenvolve exemplo de CSS inline dinamico

--- aulas_udemy/4_css_react/src/App.jsx
+++ aulas_udemy/4_css_react/src/App.jsx
@@ -1,8 +1,12 @@
 
+import { useState } from 'react'
 import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
+
+  const n = 15
+  const [name] = useState("Gabriel")
 
   return (
       <div className='App'>
@@ -15,6 +19,11 @@ function App() {
 
         {/* CSS inline */}
         <p style={{color: 'white', padding: '25px', borderTop: '2px solid black'}}>Este elemento foi estilisado de forma inline</p>
+
+        {/* CSS inline dinamico */}
+        <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS dinamico</h2>
+        <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS dinamico</h2>
+        <h2 style={name === 'Gabriel' ? ({color: "purple", backgroundColor: 'green'}) : ({color: "pink"})}>CSS dinamico</h2>
 
       </div>
   )
