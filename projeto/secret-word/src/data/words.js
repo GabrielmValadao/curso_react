17ba1c55940f550b33908757b4ac59563ef5bed3
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-16 15:17:22 -0300

    feat: reseta o projeto para iniciar desenvolvimento

---
+++ projeto/secret-word/src/data/words.js
@@ -0,0 +1,15 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Radiador", "Pistão", "Pneu", "Radio"],
+  fruta: ["Banana", "Maçã", "Abacaxi", "Mamão", "Laranja", "Uva"],
+  corpo: ["Braço", "Perna", "Orelha", "Dedos", "Pé", "Cabeça"],
+  computador: ["Mouse", "Teclado", "Monitor", "Memoria ram", "Ssd", "Gabinete"],
+  programação: [
+    "Linguagem",
+    "Framework",
+    "JavaScript",
+    "React",
+    "FrontEnd",
+    "Css",
+  ],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Macarrão"],
+};
