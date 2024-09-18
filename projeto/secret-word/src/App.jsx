f4e6ad9502fa36c438d5e17cee9583af649aabfa
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-18 20:03:14 -0300

    feat: desenvolve o random da categoria e palavra do jogo

--- projeto/secret-word/src/App.jsx
+++ projeto/secret-word/src/App.jsx
@@ -21,11 +21,46 @@ const stages = [
 function App() {
 
   const [gameStage, setGameStage] = useState(stages[0].name)
-  const [words] = useState(wordsList) 
+  const [words] = useState(wordsList)
+  
+  const [pickedWord, setPickedWord] = useState('')
+  const [pickedCategory, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    // pega uma categoria aleatória
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+
+    // pega uma palavra aleatoria da categoria
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word)
+
+    return {category, word}
+
+  }
 
   // start 
   const startGame = () => {
+    const { category, word } = pickWordAndCategory()
+
+    // cria array de letras separadas
+    let wordLetters = word.split('')
+    console.log(wordLetters)
+
+    // retira maisculo da primeira letra
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
     setGameStage(stages[1].name)
+
   }
 
   // processa a letra do input
