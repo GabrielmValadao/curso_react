e1fb284d18ee3ab89a273fcae18108acf6ca0f66
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-18 19:35:25 -0300

    feat: desenvolve outros components e importa para App.jsx

--- projeto/secret-word/src/App.jsx
+++ projeto/secret-word/src/App.jsx
@@ -1,13 +1,32 @@
+// Css
 import './App.css'
+
+// React
+import { useCallback, useEffect, useState } from 'react'
+
+// Importação de dados
+import {wordsList} from './data/words'
 
 // componentes
 import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+const stages = [
+  {id: 1, name:'start'},
+  {id: 2, name:'game'},
+  {id: 3, name:'end'},
+]
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name)
+
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   )
 }
