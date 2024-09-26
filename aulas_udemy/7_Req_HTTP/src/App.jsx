412ba859a57ddbc1bf52ba24b0464a7d9f1a7a75
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-26 15:52:04 -0300

    feat: desenvolve o resgate de dados do form com o useState

--- aulas_udemy/7_Req_HTTP/src/App.jsx
+++ aulas_udemy/7_Req_HTTP/src/App.jsx
@@ -5,9 +5,14 @@ import { useState, useEffect } from 'react';
 const url = "http://localhost:3000/products"
 
 function App() {
-  // salvando dados
+  // 1- salvando dados
   const [products, setProducts] = useState([])
-  // resgatando dados
+
+  // 2- adiciona produtos
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  // 1- resgatando dados
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(url)
@@ -18,7 +23,28 @@ function App() {
     }
 
     fetchData()
-  }, [])      
+  }, [])
+  
+  // 2- adiciona produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+
+    // requisição
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(product) 
+    })
+
+    
+  }
 
   return (
     <div className="App">
@@ -31,6 +57,27 @@ function App() {
           </li>
         ))}
       </ul>
+
+      {/* 2- adiciona produtos */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" 
+            value={name} 
+            name='name' 
+            onChange={(e) => setName(e.target.value)}/>
+          </label>
+          <label>
+            Preço:
+            <input type="text" 
+            value={price} 
+            name='price' 
+            onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
