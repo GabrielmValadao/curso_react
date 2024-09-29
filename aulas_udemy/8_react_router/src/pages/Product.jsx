3774594a0d9eda4927d240056868d770a6044be8
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-09-29 16:31:23 -0300

    feat: desenvolve carregamento de dados individuais

--- aulas_udemy/8_react_router/src/pages/Product.jsx
+++ aulas_udemy/8_react_router/src/pages/Product.jsx
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
 
 const Product = () => {
     // Rota dinamica
     const {id} = useParams()
 
-  return (
+    // carrega dado individual
+    const url = 'http://localhost:3000/products/' + id
+    const {data: product, loading, error} = useFetch(url)
 
+  return (
+    <>
     <p>ID do produto: {id}</p>
+    {error && <p>Ocorreu um erro</p>}
+    {loading && <p>Carregando dados...</p>}
+    {product && (
+        <div>
+            <h2>{product.name}</h2>
+            <h2>R$: {product.price}</h2>
+        </div>
+    )}
+    </>
     
   )
 }
