bc301855a11d034b9c5db4403bc9c5b5b7b70809
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-10-01 14:18:29 -0300

    feat: adiciona exemplo de not found

---
+++ aulas_udemy/8_react_router/src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1>
+            <h4>404</h4>
+            <p>Página não encontrada</p>
+        </h1>
+    </div>
+  )
+}
+
+export default NotFound
