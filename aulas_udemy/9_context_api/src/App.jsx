204950aab2740be9031890ee229ba785f8e4de1c
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-10-02 09:33:09 -0300

    feat: desenvolve desafio da seção

--- aulas_udemy/9_context_api/src/App.jsx
+++ aulas_udemy/9_context_api/src/App.jsx
@@ -1,10 +1,30 @@
+// import react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// style
 import './App.css'
+
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+// import components
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
       <h1>Context API - React</h1>  
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      </BrowserRouter>
      </div>
     
   )
