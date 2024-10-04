a1b08eb2437dfec17e9ca9efe8a4259eb10a9faa
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-10-04 16:13:00 -0300

    feat: desenvolve o formulario de cadastro de usuarios

---
+++ projeto/mini_blog/src/pages/Register/Register.jsx
@@ -0,0 +1,49 @@
+// import style
+import styles from './Register.module.css'
+
+import {useState, useEffect} from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe suas histórias!</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input type="text" name='name' required placeholder='Nome do Usuário' />
+        </label>
+        
+        <label>
+          <span>E-mail:</span>
+          <input 
+          type="email" 
+          name='email' 
+          required 
+          placeholder='E-mail do Usuário' />
+        </label>
+        
+        <label>
+          <span>Senha:</span>
+          <input 
+          type="password" 
+          name='password' 
+          required 
+          placeholder='Informe sua Senha' />
+        </label>
+
+        <label>
+          <span>Confirmação de senha:</span>
+          <input 
+          type="password" 
+          name='confirmPassword' 
+          required 
+          placeholder='Confirme sua Senha' />
+        </label>
+        <button className='btn'type='submit'>Cadastrar</button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
