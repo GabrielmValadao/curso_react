5a8e09212aae976ee4ca0dc592c0fe23767e87ac
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-10-04 15:56:22 -0300

    styles: desenvolve estilização geral do projeto

--- projeto/mini_blog/src/App.jsx
+++ projeto/mini_blog/src/App.jsx
@@ -8,7 +8,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
-
 // import pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About' 
