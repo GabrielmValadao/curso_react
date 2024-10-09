7ba8d569997637c0fee93318c9e5fcfdb5475743
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-10-09 10:19:03 -0300

    build: altera configuração do firebase, após atualização

--- projeto/mini_blog/src/firebase/config.jsx
+++ projeto/mini_blog/src/firebase/config.jsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7E8AOKP05L_vE2kvHmzcPnJ7Muf7kIQk",
@@ -9,7 +10,7 @@ const firebaseConfig = {
   storageBucket: "miniblog-03do10.appspot.com",
   messagingSenderId: "604171607788",
   appId: "1:604171607788:web:581459ea1696fe26578fc9",
-  measurementId: "G-FQLTP2XRRZ"
+  measurementId: "G-FQLTP2XRRZ",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,4 +18,6 @@ const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
 
-export { db };
+const auth = getAuth(app);
+
+export { db, auth };
